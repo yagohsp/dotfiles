12c75563b1d23be4f7389ae2562db7fe2e64f194
Yago Henrique <yago.gif@gmail.com>
2024-11-25 18:48:25 -0300

    Change dir

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
 		"stevearc/oil.nvim",
 		config = function()
 			require("oil").setup({
+                view_options = {
+                    show_hidden = true
+                },
 				keymaps = {
 					["<C-p>"] = false,
 				},
--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-oh-my-posh init pwsh --config '~\AppData\Local\Programs\oh-my-posh\themes\zash.omp.json' | Invoke-Expression
-
-Set-PSReadLineKeyHandler -Key ctrl+f -Function AcceptSuggestion
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-
-$dotfiles = "~\.dotfiles"
-$nvim = "~\.dotfiles\nvim"
-$tmux = "~\.dotfiles\tmux"
-$hp = "D:\HappyPlace"
