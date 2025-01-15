5a378f33d4951a9bb743180154a5f183bfeb1900
Yago Henrique <yago.santos@gowsolucoes.com>
2025-01-15 13:38:54 -0300

    fish

---
+++ fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/yago/.nix-profile/etc/profile.d/nix.fish; . /home/yago/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g PATH ~/.dotnet $PATH
+set -g PATH ~/.dotnet/tools $PATH
+
+set -g PATH ~/.local/share/nvim/mason/bin $PATH
+
+
