833f169c2d878364b034f4d4dae1d92ed3dbd34c
Yago Henrique <yago.gif@gmail.com>
2025-01-19 16:51:42 -0300

    notify

---
+++ nvim/lua/plugins/others.lua
@@ -0,0 +1,6 @@
+return {
+    { 'echasnovski/mini.nvim', version = false },
+    "jiangmiao/auto-pairs",
+    "tpope/vim-commentary",
+    "rcarriga/nvim-notify"
+}
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,59 +1,59 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "catppuccin",
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "buffers" },
-					lualine_x = { "filetype" },
-					lualine_y = {},
-					lualine_z = { "location" },
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = { "filename" },
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
-				},
-			})
-		end,
-	},
-	{
-		"VonHeikemen/fine-cmdline.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("fine-cmdline").setup({
-				popup = {
-					position = {
-						row = "80%",
-					},
-					size = {
-						width = "40%",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"xiyaowong/transparent.nvim",
-	},
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        priority = 1000,
+        name = "catppuccin",
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "catppuccin",
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { "buffers" },
+                    lualine_x = { "filetype" },
+                    lualine_y = {},
+                    lualine_z = { "location" },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { "filename" },
+                    lualine_x = { "location" },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+            })
+        end,
+    },
+    {
+        "VonHeikemen/fine-cmdline.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("fine-cmdline").setup({
+                popup = {
+                    position = {
+                        row = "80%",
+                    },
+                    size = {
+                        width = "40%",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "xiyaowong/transparent.nvim",
+    },
 }
