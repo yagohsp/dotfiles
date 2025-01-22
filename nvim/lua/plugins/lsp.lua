95861d225b135ea0aef83b7902825e9977d9e65c
Yago Henrique <yago.gif@gmail.com>
2025-01-22 08:49:29 -0300

    Arch linux

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,44 +1,42 @@
 return {
     {
-        "williamboman/mason.nvim",
-        lazy = false,
+        'williamboman/mason.nvim',
         config = function()
-            require("mason").setup()
+            require('mason').setup()
         end,
     },
     {
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason-lspconfig.nvim',
         lazy = false,
         opts = {
-            auto_install = true,
-        },
+            auto_install = true
+        }
     },
     {
-        "neovim/nvim-lspconfig",
-        lazy = false,
+        'neovim/nvim-lspconfig',
         config = function()
             local mason_lspconfig = require("mason-lspconfig")
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-            mason_lspconfig.setup({
-                ensure_installed = {
-                    "emmet-language-server",
-                    "eslint-lsp",
-                    "eslint_d",
-                    "html-lsp",
-                    "htmlhint",
-                    "json-lsp",
-                    "lua-language-server",
-                    "csharp_ls",
-                    "csharpier",
-                    "prettierd",
-                    "rust-analyzer",
-                    "stylua",
-                    "tailwindcss-language-server",
-                    "typescript-language-server",
-                },
-            })
+            mason_lspconfig.setup(
+                {
+                    ensure_installed = {
+                        "emmet_language_server",
+                        "eslint",
+                        "html",
+                        "jsonls",
+                        "lua_ls",
+                        "csharp_ls",
+                        "rust_analyzer",
+                        "tailwindcss",
+                        "ts_ls",
+                        -- "eslint_d",
+                        -- "prettierd",
+                        -- "stylua",
+                        -- "csharpier",
+                        -- "htmlhint",
+                    },
+                })
 
             mason_lspconfig.setup_handlers({
                 function(server_name)
@@ -47,17 +45,7 @@ return {
                     })
                 end,
             })
-
-            for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-                local default_diagnostic_handler = vim.lsp.handlers[method]
-                vim.lsp.handlers[method] = function(err, result, context, config)
-                    if err ~= nil and err.code == -32802 then
-                        return
-                    end
-                    return default_diagnostic_handler(err, result, context, config)
-                end
-            end
-        end,
+        end
     },
     {
         "j-hui/fidget.nvim",
