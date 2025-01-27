d37538be3712a0273a57b6064ed184bd7fe4462d
Yago Henrique dos Santos Pacheco <yago.gif@gmail.com>
2025-01-27 00:48:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # packages
-sudo pacman -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim --noconfirm
+sudo pacman -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim cmake dotnet-sdk --noconfirm
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chmod +x ~/.config/initial/*
+chmod +x ~/.config/install/*
 ~/.config/install/auto-start.sh
 ~/.config/install/fix-cedilha.sh
