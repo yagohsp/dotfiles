5e1c84e898cd907ce7b7cfc3901dd5d25c9eeeff
Yago Henrique dos Santos Pacheco <yago.gif@gmail.com>
2025-01-27 01:07:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # packages
-sudo pacman -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim cmake dotnet-sdk --noconfirm
+yay -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim cmake dotnet-sdk ttf-firacode-nerd --noconfirm
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
