0883f75c4c948a2ae5c889c62e67e7e46571d667
Yago Henrique dos Santos Pacheco <yago.gif@gmail.com>
2025-01-27 00:29:42 -0300

    Update and rename inital.sh to install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 # packages
-sudo pacman -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim
+sudo pacman -S cliphist wtype zsh-syntax-highlighting hyprlock waybar wofi kitty xclip zsh neovim --noconfirm
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 chmod +x ~/.config/initial/*
-~/.config/initial/auto-start.sh
-~/.config/initial/fix-cedilha.sh
+~/.config/install/auto-start.sh
+~/.config/install/fix-cedilha.sh
