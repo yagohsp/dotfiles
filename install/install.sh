a4079963ef82153226cb0b60cfc1ab8623edb5a6
Yago Henrique <yago.santos@gowsolucoes.com>
2025-01-29 17:31:36 -0300

    desktop theme

--- install/install.sh
+++ install/install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/ya
 # packages
 yay -S uwsm httpie-desktop fzf blueman cliphist wtype zsh-syntax-highlighting hyprlock \
     waybar wofi kitty xclip zsh neovim cmake dotnet-sdk ttf-firacode-nerd npm pnpm nodejs \
-    firefox swappy wl-clipboard --noconfirm 
+    firefox swappy wl-clipboard imv viu --noconfirm 
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
