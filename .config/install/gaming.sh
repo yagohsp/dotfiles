5198f1e7ce18b353864e8a47cd08145ac703195a
Yago Henrique <yago.gif@gmail.com>
2025-07-09 11:43:58 -0300

    att install; att gtk; att hyprpaper

---
+++ .config/install/gaming.sh
@@ -0,0 +1,8 @@
+# in /etc/pacman.conf
+# uncomment
+## [multilib]
+## Include = /etc/pacman.d/mirrorlist
+
+# sudo pacman -Syu
+# sudo pacman -S ttf-liberation lib32-nvidia-utils
+#
--- .config/install/install.sh
+++ .config/install/install.sh
@@ -1,4 +1,5 @@
 sudo pacman -S git
+cd ~
 git clone https://github.com/yagohsp/dotfiles.git
 
 # yay
@@ -6,12 +7,13 @@ sudo pacman -S --needed base-devel && git clone https://aur.archlinux.org/yay.gi
 
 # packages
 yay -S uwsm httpie-desktop cliphist wtype zsh-syntax-highlighting hyprlock hyprpaper \
-    waybar wofi kitty xclip zsh neovim cmake dotnet-sdk ttf-firacode-nerd npm pnpm nodejs \
-    firefox swappy wl-clipboard fastfetch nvm \
+    waybar rofi kitty xclip zsh neovim cmake dotnet-sdk ttf-firacode-nerd npm pnpm nodejs \
+    zen-browser-bin swappy wl-clipboard fastfetch nvm \
     galculator archlinux-xdg-menu hyprpicker wl-clipboard dunst lazygit tmux wayfreeze \
     yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick stow \
     fcitx5 fcitx5-im fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-mozc \
-    ark unrar
+    ark unrar nautilus legcord libnotify bibata-cursor-theme noto-fonts-cjk ttf-jetbrains-mono-nerd \
+    vlc vlc-plugin-ffmpeg
 --noconfirm
 
 bash <(curl -s https://raw.githubusercontent.com/quantumvoid0/better-control/refs/heads/main/betterctl.sh)
@@ -19,7 +21,8 @@ bash <(curl -s https://raw.githubusercontent.com/quantumvoid0/better-control/ref
 sudo systemctl disable sddm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
 chmod -R +x ~/.config
 ~/.config/install/auto-start.sh
@@ -28,3 +31,9 @@ chmod -R +x ~/.config
 sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo touch /etc/systemd/system/getty@tty1.service.d/autologin.conf
 sudo bash -c 'echo -e "[Service]\nExecStart=\nExecStart=-/sbin/agetty -o '\''-p -f -- \\u'\'' --noclear --autologin '$WHOAMI' %I \$TERM"' > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+
+cd ~/dotfiles
+stow --override='.*' .
+
+cd ~
+rm -rf yay
