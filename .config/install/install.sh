331a0a3c7fca711eee5c60193dbd3585de221020
Yago Henrique <yago.santos@gowsolucoes.com>
2025-08-22 09:24:35 -0300

    att minimal

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -3,13 +3,13 @@ sudo pacman -S git --noconfirm
 cd ~
 git clone https://github.com/yagohsp/dotfiles.git
 
-# 2. Install yay 
+# 2. Install yay
 sudo pacman -S --needed base-devel --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 
-# 3. Install packages 
+# 3. Install packages
 yay -S --noconfirm \
     kitty zsh neovim tmux lazygit \
     jq poppler fd ripgrep fzf zoxide imagemagick fastfetch \
@@ -22,21 +22,21 @@ yay -S --noconfirm \
     cliphist wtype wl-clipboard xclip \
     fcitx5 fcitx5-im fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-mozc \
     zen-browser-bin google-chrome httpie-desktop ffmpeg \
-    flatpak vial rustup qt6ct unzip
+    flatpak vial rustup qt6ct unzip bat
 
-flatpak install -y vlc  
+flatpak install -y vlc
 
-# 4. Install tmux plugin manager 
+# 4. Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# 5. Install Better control 
+# 5. Install Better control
 bash <(curl -s https://raw.githubusercontent.com/quantumvoid0/better-control/refs/heads/main/betterctl.sh)
 
 # 6. Disable SDDM to skip
 sudo systemctl disable sddm
 
 # 7. Install Oh My Zsh and plugins
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
@@ -55,4 +55,4 @@ stow --override='.*' .
 
 # 11. Cleanup yay directory
 cd ~
-rm -rf yay  
+rm -rf yay
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,33 +1,26 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local actions = require("telescope.actions")
-            require("telescope").setup({
-                pickers = {
-                    find_files = {
-                        hidden = true
-                    }
-                },
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<S-Tab>"] = actions.move_selection_next,
-                            ["<Tab>"] = actions.move_selection_previous,
-                        },
-                    },
-                    file_ignore_patterns = { ".git/" },
-                },
-                dependencies = {
-                    "BurntSushi/ripgrep",
-                },
-            })
-        end,
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
-    },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        },
+        defaults = {
+          file_ignore_patterns = { ".git/" },
+        },
+        dependencies = {
+          "BurntSushi/ripgrep",
+        },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+  },
 }
