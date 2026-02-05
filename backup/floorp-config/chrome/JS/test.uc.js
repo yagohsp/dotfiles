8c113fbf1bc4738144bd5291263a9bc3513f68e9
Yago Henrique <yago.gif@gmail.com>
2026-02-04 23:26:38 -0300

    att userChrome

--- backup/floorp-config/chrome/JS/test.uc.js
+++ backup/floorp-config/chrome/JS/test.uc.js
@@ -2,14 +2,27 @@ const sidebar = document.getElementsByClassName("sidebar-browser-stack")[0];
 const sidebarBox = document.getElementById("sidebar-box");
 const tabBrowser = document.getElementById("tabbrowser-tabbox");
 const toolbar = document.getElementById("navigator-toolbox");
+const browser = document.getElementById("browser");
 
-sidebarBox.addEventListener("mouseenter", function () {
-  sidebar.classList.add("open");
+let mouse_on_left = false;
+
+browser.addEventListener("pointermove", function (e) {
+  if (e.clientX < 150) {
+    mouse_on_left = true;
+  } else {
+    mouse_on_left = false;
+  }
 });
 
-tabBrowser.addEventListener("mouseenter", function () {
+browser.addEventListener("pointerleave", function () {
+  if (mouse_on_left) {
+    sidebar.classList.add("open");
+  }
+});
+
+tabBrowser.addEventListener("pointerenter", function () {
   sidebar.classList.remove("open");
 });
-toolbar.addEventListener("mouseenter", function () {
+toolbar.addEventListener("pointerenter", function () {
   sidebar.classList.remove("open");
 });
